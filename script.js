48ad355be3526bb82062b0a8402c1cdd6406d170
Ali Hamza <alihamzaahmad680@gmail.com>
2026-03-26 11:11:06 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const API_KEY = "14c6ced6bb6c43309e4b6b4cd2a83e7d";
+const url = "https://newsapi.org/v2/everything?";
+
+window.addEventListener('load', () => fetchNews('pakistan'));
+
+function reload() {
+  window.location.reload();
+}
+async function fetchNews(query) {
+  const res = await fetch(`${url}q=${query}&apiKey=${API_KEY}`);
+  const data = await res.json();
+  bindData(data.articles);
+}
+function bindData(articles) {
+  const cardContainer = document.getElementById('card-container');
+  const newsCardTemplate = document.getElementById('template-news-card');
+
+  cardContainer.innerHTML = '';
+
+  articles.forEach(article => {
+    if (!article.urlToImage) return;
+
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+
+    cardClone.querySelector('.news-img').src = article.urlToImage;
+    cardClone.querySelector('.news-title').innerText = article.title;
+    cardClone.querySelector('.news-source').innerText = article.source.name;
+    const newsdesc = cardClone.querySelector("#news-desc")
+    const card = cardClone.firstElementChild;
+    card.addEventListener("click", () => {
+      window.open(article.url, "_blank");
+    });
+    cardContainer.appendChild(cardClone);
+  });
+};
+
+async function onNavItemClick(id) {
+  await fetchNews(id);
+}
+const searchButton = document.getElementById("search-button")
+const searchText = document.getElementById("search-text")
+searchButton.addEventListener("click", () => {
+  const query = searchText.value;
+  if (!query) return
+  fetchNews(query);
+
+});
+
+const mobileMenu = document.getElementById("mobile-menu");
+
+function openMenu() {
+  mobileMenu.classList.remove("translate-x-full");
+}
+
+function closeMenu() {
+  mobileMenu.classList.add("translate-x-full");
+}
+
+function mobileSearch() {
+  const query = document.getElementById("search-text-mobile").value;
+  if (!query) return;
+  fetchNews(query);
+  closeMenu();
+}
+
+
+
+
+
+
+
+
+
